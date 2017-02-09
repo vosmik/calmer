cfd73ddfce2ee313ec3b10576330b652664e676b
Jan Vosmik <hvosmik@gmail.com>
2017-02-09 13:14:23 +0100

    replace ENV with Figaro.env in model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post
 	base_uri 'https://api.500px.com/v1/photos'
 	default_params 	only: "nature", rpp: "12", 
 					image_size: "600", 
-					consumer_key: ENV["PX_CONSUMER_KEY"]
+					consumer_key: Figaro.env.px_consumer_key
 
 	def self.for feature
 		get("", query: {feature: feature}, verify: false).parsed_response
